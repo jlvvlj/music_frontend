42752e9f7acbdea493d806420791dd030bf59a86
devstack03 <devking0325@gmail.com>
2023-11-23 08:11:25 +0200

    feat: add drag and drop input form

--- src/components/ui/upload-file.tsx
+++ src/components/ui/upload-file.tsx
@@ -1,12 +1,41 @@
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, PromiseLikeOfReactNode } from "react"
+import { useDropzone, FileWithPath } from "react-dropzone";
+import { ArrowUpTrayIcon } from "@heroicons/react/20/solid";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  ReactElement,
+  JSXElementConstructor,
+  ReactNode,
+  ReactPortal,
+  PromiseLikeOfReactNode,
+} from "react";
 
 export default function UploadFile(props: { input: string }) {
-    return (
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor="input">{props.input}</Label>
-            <Input id="input" type="file" />
-        </div>
-    )
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+  const files = acceptedFiles.map((file: FileWithPath) => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+  return (
+    <>
+      <div
+        {...getRootProps({
+          className: "dropzone grid w-full max-w-sm items-center gap-1.5",
+        })}
+      >
+        <Label htmlFor="input">{props.input}</Label>
+        <Input id="input" type="file" {...getInputProps()} />
+
+        <p className="border border-solid border-white p-2 rounded-md mt-1 flex items-center gap-5">
+          <ArrowUpTrayIcon className="h-5 w-5 text-white" aria-hidden="true" />
+          Drop or Select file
+        </p>
+      </div>
+      <div>
+        <ul>{files}</ul>
+      </div>
+    </>
+  );
 }
