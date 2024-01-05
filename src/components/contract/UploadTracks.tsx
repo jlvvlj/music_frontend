3381cf04c6edfb3a187b76b68be088c19c75a1c6
harshil-bodara <developer.harshilbodara@gmail.com>
2024-01-05 17:25:54 +0530

    design changes in contributors step

--- src/components/contract/UploadTracks.tsx
+++ src/components/contract/UploadTracks.tsx
@@ -76,7 +76,7 @@ export default function UploadTracks({ updateStep, selectedFile, setSelectedFile
                         value={updateValue}
                         onChange={(e) => setUpdateValue(e.target.value)}
                     />
-                    <Card className="bg-modal">
+                    <Card className="bg-modal mb-8">
                         <CardHeader className="flex-row gap-3 space-y-0 items-center py-2.5">
                             <UploadCloud className="h-5 w-5" />
                             <div>
@@ -85,12 +85,12 @@ export default function UploadTracks({ updateStep, selectedFile, setSelectedFile
                             </div>
                         </CardHeader>
                         <Separator />
-                        <CardContent className="py-5 grid grid-cols-2 gap-4">
-                            <div className="bg-modal border border-3 py-5 px-2.5 rounded-xl relative">
+                        <CardContent className="py-5 gap-4">
+                            <div className="bg-modal border border-3 py-20 px-2.5 rounded-xl relative">
                                 <div>
                                     <UploadCloud className="h-5 w-5 mb-4 mx-auto" />
-                                    <h6 className="text-[13px]">Choose a file or drag & drop it here</h6>
-                                    <p className="text-[#737373] text-[13px] mx-4">Only jpg or png. Up to 50 MB.</p>
+                                    <h6 className="text-[13px] text-center">Choose a file or drag & drop it here</h6>
+                                    <p className="text-[#737373] text-[13px] mx-4 text-center">Only jpg or png. Up to 50 MB.</p>
                                     <div className="text-center mt-2">
                                         <label className="text-[11px] bg-modal p-1 h-[34px] border border-border3 rounded-xl py-2 cursor-pointer">
                                             Select Cover
@@ -101,17 +101,29 @@ export default function UploadTracks({ updateStep, selectedFile, setSelectedFile
                                 {selectedFile && (
                                     <div>
                                         <div className="bg-modal rounded-xl selected-image-container z-20 absolute h-full w-full inset-0 flex items-center justify-center">
-                                            <img src={selectedFile} alt="Selected Image" className="h-full w-full object-fit" />
+                                            <img src={selectedFile} alt="Selected Image" className="h-full w-full object-cover rounded-xl" />
                                             <Button onClick={handleRemoveFile} className="absolute top-1 right-1 h-5 w-5 flex items-center justify-center shadow-sm rounded-full p-0"><XMarkIcon className="text-black3" /></Button>
                                         </div>
                                     </div>
                                 )}
                             </div>
-                            <div className="bg-modal border border-3 py-5 px-2.5 rounded-xl relative">
+                        </CardContent>
+                    </Card>
+                    <Card className="bg-modal">
+                        <CardHeader className="flex-row gap-3 space-y-0 items-center py-2.5">
+                            <UploadCloud className="h-5 w-5" />
+                            <div>
+                                <h6 className="text-base">Upload your files</h6>
+                                <p className="text-xs">Drag and drop your Cover and Tracks Folder.</p>
+                            </div>
+                        </CardHeader>
+                        <Separator />
+                        <CardContent className="py-5 gap-4">
+                            <div className="bg-modal border border-3 py-5 px-2.5 rounded-xl relative mb-6">
                                 <div>
                                     <UploadCloud className="h-5 w-5 mb-4 mx-auto" />
-                                    <h6 className="text-[13px]">Choose a file or drag & drop it here</h6>
-                                    <p className="text-[#737373] text-[13px] mx-4">Only mp3 or aac. Up to 50 MB.</p>
+                                    <h6 className="text-[13px] text-center">Choose a file or drag & drop it here</h6>
+                                    <p className="text-[#737373] text-[13px] mx-4 text-center">Only mp3 or aac. Up to 50 MB.</p>
                                     <div className="text-center mt-2">
                                         <label className="text-[11px] bg-modal p-1 h-[34px] border border-border3 rounded-xl py-2 cursor-pointer">
                                             Select Audio
@@ -119,18 +131,25 @@ export default function UploadTracks({ updateStep, selectedFile, setSelectedFile
                                         </label>
                                     </div>
                                 </div>
-                                {selectedAudio && (
-                                    <div>
-                                        <div className="bg-modal rounded-xl selected-image-container z-20 absolute h-full w-full inset-0 flex items-center justify-center">
-                                            <audio controls className="w-full mx-2.5 border border-border3 rounded-[30px]">
-                                                <source src={selectedAudio} type="audio/mp3" />
-                                            </audio>
-                                            <Button onClick={handleRemoveAudio} className="absolute top-1 right-1 h-5 w-5 flex items-center justify-center shadow-sm rounded-full p-0"><XMarkIcon className="text-black3" /></Button>
-                                        </div>
-                                    </div>
-                                )}
-
                             </div>
+                            {selectedAudio && (
+                                <div className="space-y-6 bg-modal p-4 rounded-xl border ">
+                                    <div className="bg-modal rounded-xl selected-image-container z-20 h-full w-full flex items-center justify-between">
+                                        <audio
+                                            controls
+                                            className="w-[calc(100%-30px)] border border-border3 rounded-[30px]"
+                                        >
+                                            <source src={selectedAudio} type="audio/mp3" />
+                                        </audio>
+                                        <Button
+                                            onClick={handleRemoveAudio}
+                                            className="h-4 w-4 flex items-center justify-center shadow-sm rounded-full p-0 mt-1.5"
+                                        >
+                                            <XMarkIcon className="text-black3" />
+                                        </Button>
+                                    </div>
+                                </div>
+                            )}
                         </CardContent>
                     </Card>
                 </div>
